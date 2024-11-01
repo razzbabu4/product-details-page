6c37247b969bc5064286eb383b21f781daa025e8
Tareq <razzbabut@gmail.com>
2024-11-01 20:44:41 +0600

    complete initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProductDetailsPage from "./components/ProductDetailsPage"
+
+function App() {
+  return (
+    <>
+      <ProductDetailsPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ProductDetailsPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ProductDetailsPage = () => {
+    return (
+        <div>
+           <h1>This Is Product Details Page</h1> 
+        </div>
+    );
+};
+
+export default ProductDetailsPage;
